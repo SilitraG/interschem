2afe35d1a79c7f4cfc9bb4618cc13a7c088036a4
Victor Grosu <48522466+vagrosu@users.noreply.github.com>
2021-12-09 18:34:03 +0200

    Main structures remake

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <cstring>
-#include "victor.h"
-#include "florin.h"
-
-// Example video: https://www.youtube.com/watch?v=uxsv5s4Gcw0
-using namespace std;
-
-//Block type
+//Block type Id
 #define EMPTY_BLOCK 0
 #define START_BLOCK 1
 #define INPUT_BLOCK 2
@@ -15,31 +7,60 @@ using namespace std;
 #define DECISION_BLOCK 5
 #define STOP_BLOCK 6
 
-struct variables {
-    int id;
-    char name[30];
-    long int value;
-}var[101];
+#include <iostream>
 
-struct logicBlocks {
-    float xPos, yPos;
-    int typeId;
-    char typeName[10];
+// Example video: https://www.youtube.com/watch?v=uxsv5s4Gcw0
+using namespace std;
+
+
+struct Variables {
+    int varsNumber=0; //Total number of variables
+    struct Variable {
+        //int id;
+        char name[30]; //Variable name
+        long double value; //Variable value
+    }Var[50];
+}Vars;
+
+struct LogicBlock {
+    float xPos, yPos; //Position of the block
+    int typeId=0; //Block type (check def)
+    //char typeName[10]=NULL; //Block name
+    int varId=-1; //Variable ID for INPUT_BLOCK, ASSIGN_BLOCK and OUTPUT_BLOCK
+    char varFullExpression[50]={NULL}; //Full math expression for INPUT_BLOCK, ASSIGN_BLOCK and OUTPUT_BLOCK
+    LogicBlock *next=NULL; //Next block
+    LogicBlock *tru=NULL; //Next block if true (for DECISION_BLOCK)
+    LogicBlock *fls=NULL; //Next block if false (for DECISION_BLOCK)
 };
+
+struct BlocksList {
+    int numberOfBlocks = 0;
+    LogicBlock *first; //First block of the code
+    LogicBlock *last; //Last block of the code
+}code;
+
+#include "victor.h"
+#include "florin.h"
 
 int main()
 {
+///////////////// TESTE VICTOR /////////////////
     int blockType = EMPTY_BLOCK;
     char blockName[10];
 
+    addStartBlock();
+
+     /*
     while(blockType != STOP_BLOCK) {
         cout << "Introduceti instructiunea: ";
         cin >> blockName;
-        blockType = blockTypeDecider(blockName);
-        //blockTypeCaller(blockType);
+        blockType = blockNameToTypeConverter(blockName);
+        blockTypeLogicCaller(blockType);
     }
-    
-    /////verificare functie postfixat
+     */
+     
+ ///////////////// TESTE FLORIN /////////////////
+ /*   /////verificare functie postfixat
     char s[256];
 
     strcpy(s,"3+2*(1+5)+7");
@@ -47,6 +68,7 @@ int main()
     ///Ar trebui sa obtin 3215+*+7+
 
     calcul_expresie_f(s);
-
+    
+*/
     return 0;
 }
